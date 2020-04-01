400ba080bcdbd973e8eddbef826745304f269b41
CohlHatch <60143723+CohlHatch@users.noreply.github.com>
2020-03-31 23:25:05 -0500

    update 3

--- ch15_ex2/cart/index.php
+++ ch15_ex2/cart/index.php
@@ -18,16 +18,34 @@ if ($action === NULL){
     $action = 'reset';
 } else {
     $action = strtolower($action);
-    header("Location:.?category_id=$category_id");
-} else if ($action =='show_add_form') {
+
+} else if  ($action =='show_add_form') {
     $code = "";
     $name = "";
     $price = "";
     $categories = CategoryDB::getCategories();
     include('product_add.php');
 } else if ($action == 'add_product') {
+    $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+    $code = filter_input(INPUT_POST,'code');
+    $name = filter_input(INPUT_POST, 'name');
+    $price = filter_input(INPUT_POST, 'price')
 
+        $validate->text('code',$code,true, 1, 10);
+    $validate->text('name', $name);
+    $validate->number('price', $price);
 
+    if ($fields->hasErrors()) {
+        $categories = CategoryDB::getCategories();
+        include 'product_add.php';
+
+    } else {
+        $current_category = CategoryDB::getCategories($category_id);
+        $product = new Product($current_category, $code, $name, $price);
+        ProductDB::addProduct($product);
+
+        header("Location:.?category_id=$category_id");
+    }
 }
 
 
